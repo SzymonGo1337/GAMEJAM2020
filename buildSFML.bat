d388ede4eb58f9366a3609808f4adebb1140ff82
SzymonGo1337 <46822941+SzymonGo1337@users.noreply.github.com>
2020-07-28 11:23:26 +0200

    first commit

---
+++ buildSFML.bat
@@ -0,0 +1,2 @@
+g++ main.cpp -o build\app -ISFML\include -LSFML\lib -lsfml-graphics -lsfml-window -lsfml-system
+pause
---
+++ buildSFML.sh
@@ -0,0 +1,1 @@
+g++ main.cpp -o build/app -ISFML/include -LSFML/lib -lsfml-graphics -lsfml-window -lsfml-system
