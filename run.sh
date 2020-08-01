f597c15f6df0f2b3233c8e0cb619c71be84469b9
SzymonGo1337 <46822941+SzymonGo1337@users.noreply.github.com>
2020-08-01 15:37:20 +0200

    sliders, sliders, and more sliders

--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-APP_NAME=a.out
-
-cd bin/
-./"$APP_NAME"
--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,28 @@
-#include "physics.hpp"
+#include <SFML/Graphics.hpp>
+#include "gui/gui.hpp"
+
+void test(float var) {
+	std::cout << var << std::endl;
+}
 
 int main(){
-	sf::RenderWindow* window = new sf::RenderWindow(sf::VideoMode(800, 600), "SFML works!");
+	sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
 
-	sf::RectangleShape rect;
-	rect.setSize(sf::Vector2f(20.0f, 20.0f));
-	rect.setPosition(50.0f, 50.0f);
-	rect.setFillColor(sf::Color::Cyan);
+	Slider s1(10.0f, 10.0f, sf::Color::Cyan, sf::Color::White);
 
-	Box b1(100.0f, 100.0f, sf::Vector2f(20.0f, 20.0f), sf::Color::Cyan);
-
-	while (window->isOpen()){
+	while (window.isOpen()){
 		sf::Event event;
-		while (window->pollEvent(event)){
+		while (window.pollEvent(event)){
 			if (event.type == sf::Event::Closed)
-				window->close();
+				window.close();
 		}
 
-		window->clear();
+		window.clear();
 
-		//window->draw(rect);
-		b1.physics(0.0f, 0.1f, 1.0f);
-		b1.draw(*window);
+		s1.clicked(window, test);
+		s1.draw(window);
 
-		window->display();
+		window.display();
 	}
 
 	return 0;
