12d15661f5cc748dac2bc02f727e85d925cd1d40
Patryk Paluch <patrykas1000@gmail.com>
2020-08-01 06:39:46 +0200

    Bugfix colorGradient and Add evaluate()

--- src/components/colorGradient.cpp
+++ src/components/colorGradient.cpp
@@ -1,9 +1,45 @@
 #include "colorGradient.hpp"
-//TODO: tests
 
-ColorGradient::ColorGradient() {}
+ColorGradient::GradientKey::GradientKey() : // default
+	position(-1),
+	color(0,0,0) {}
+
+ColorGradient::GradientKey::GradientKey(const GradientKey &org) : // copy
+	position(org.position),
+	color(org.color) {} 
+
+ColorGradient::GradientKey::GradientKey(float pPosition, sf::Color pColor) : // parametric
+	position(pPosition),
+	color(pColor) {}
+
+ColorGradient::ColorGradient(){
+}
+
+ColorGradient::ColorGradient(std::initializer_list<sf::Color> colors) {
+	unsigned int size = colors.size();
+	if(size <= 0) return;
+
+	values.resize(colors.size());
+	if(size == 1) { // special case
+		values[0].position = 0;
+		values[0].color = *(colors.begin());
+		return;
+	}
+
+	float step = 1.0f / (size-1);
+	int i = 0;
+
+	std::initializer_list<sf::Color>::iterator it;
+	for(it = colors.begin() ; it != colors.end() ; ++it, ++i){
+		values[i].position = step * i;
+		values[i].color = *it;
+	}
+}
 
 void ColorGradient::addKey(float position, sf::Color color){
+	if(position < 0) position = 0;
+	else if(position > 1) position = 1;
+
 	std::vector<GradientKey>::iterator it = values.begin();
 	bool notInserted = true;
 	while (it < values.end()) {
@@ -17,6 +53,7 @@ void ColorGradient::addKey(float position, sf::Color color){
 	if(notInserted){
 		values.push_back(GradientKey(position, color));
 	}
+
 }
 
 int ColorGradient::getKeysCount(){
@@ -24,28 +61,37 @@ int ColorGradient::getKeysCount(){
 }
 
 // undefined behavior for values.size() == 0
-ColorGradient::GradientKey ColorGradient::getKey(int index){
-	if(index < 0) index = 0;
-	else if(index >= values.size()) index = values.size() - 1;
+ColorGradient::GradientKey ColorGradient::getKey(unsigned int index){
+	if(index >= values.size()) index = values.size() - 1;
 	return values[index];
 }
 
+void ColorGradient::setKey(unsigned int index, float position, sf::Color color){
+	if(index >= values.size()) return;
 
-void ColorGradient::setKey(int index, float position, sf::Color color){
-	if(index < 0 || index > values.size()) return;
-
-	values[index].position = position;
-	values[index].color = color;
+	float left = -1;
+	float right = 2;
+	if(index > 0) left = values[index-1].position;
+	else if(index+1 < values.size()) right = values[index+1].position;
+	
+	// Check if point will be in corect position after change
+	if(left <= position && position <= right){
+		values[index].position = position;
+		values[index].color = color;
+	}
+	else { 
+		// lazy position correction (remove & insert)
+		values.erase(values.begin()+index);
+		addKey(position, color);
+	}
 }
 
-void ColorGradient::setKey(int index, float position){
-	if(index < 0 || index > values.size()) return;
-
-	values[index].position = position;
+void ColorGradient::setKey(unsigned int index, float position){
+	setKey(index, position, values[index].color);
 }
 
-void ColorGradient::setKey(int index, sf::Color color){
-	if(index < 0 || index > values.size()) return;
+void ColorGradient::setKey(unsigned int index, sf::Color color){
+	if(index >= values.size()) return;
 
 	values[index].color = color;
 }
@@ -56,7 +102,48 @@ int ColorGradient::getAllKeys(GradientKey *array){
 	}
 }
 
-void ColorGradient::removeKey(int index){
-	if(index < 0 || index >= values.size()) return;
+void ColorGradient::removeKey(unsigned int index){
+	if(index >= values.size()) return;
 	values.erase(values.begin() + index);
 }
+
+unsigned int ColorGradient::getLeftKeyIndexFor(float position){
+	unsigned int last = 0;
+	// gradients do not have a lot of points so O(n) is good enough
+	for(int i = 0 ; i < values.size() && values[i].position < position; i++){
+		last = i;
+	}
+	
+	return last;
+}
+
+// Not in ColorGradient. Just for use in ColorGradient::evaluate
+inline sf::Uint8 LerpColorComponent(sf::Uint8 start, sf::Uint8 end, float t){
+	return (sf::Uint8)((end - start) * t + start);
+}
+
+sf::Color ColorGradient::evaluate(float position){
+	unsigned index = getLeftKeyIndexFor(position);
+
+	if(index + 1 >= getKeysCount()){ //its last point
+		return values[index].color;
+	}
+
+	GradientKey startKey = values[index];
+	GradientKey endKey = values[index+1];
+
+	sf::Color startColor = startKey.color;
+	sf::Color endColor = endKey.color;
+
+	// map position in [start ; end] to position in [0 ; 1]
+	float t = (position - startKey.position) * (1.0f / (endKey.position - startKey.position));
+	
+	// color = Lerp(startColor, endColor, t)
+	sf::Uint8 r = LerpColorComponent(startColor.r, endColor.r, t);
+	sf::Uint8 g = LerpColorComponent(startColor.g, endColor.g, t);
+	sf::Uint8 b = LerpColorComponent(startColor.b, endColor.b, t);
+	sf::Uint8 a = LerpColorComponent(startColor.a, endColor.a, t);
+
+	return sf::Color(r, g, b, a);
+}
+
--- src/components/colorGradient.hpp
+++ src/components/colorGradient.hpp
@@ -3,23 +3,23 @@
 #include <SFML/Graphics/Color.hpp>
 #include <vector>
 
+/**
+ * Represents gradient
+ * 
+ * Most of 'get' methods have undefined behavior for getKeysCount() == 0
+ * Make sure to do not use with "empty" gradient
+ */
 class ColorGradient {
 public:
 	struct GradientKey {
 		float position;
 		sf::Color color;
 
-		GradientKey() : // default
-			position(-1),
-			color(0,0,0) {}
+		GradientKey(); // default
 
-		GradientKey(const GradientKey &org) : // copy/clone
-			position(org.position),
-			color(org.color) {} 
+		GradientKey(const GradientKey &org); // copy
 
-		GradientKey(float pPosition, sf::Color pColor) : // parametric
-			position(position),
-			color(pColor) {}
+		GradientKey(float pPosition, sf::Color pColor); // parametric
 	};
 
 private:
@@ -28,7 +28,12 @@ private:
 
 public:
 
-	ColorGradient() {}
+	ColorGradient();
+
+	/**
+	 * Creates gradient filed with passed colors in equal distance, first color at position=0, and last one at position=1
+	 */
+	ColorGradient(std::initializer_list<sf::Color> colors);
 
 	/**
 	 * Adds new key to gradient
@@ -47,16 +52,16 @@ public:
 	 * @param index Index of key
 	 * @returns gradient key at index or first/last gradient key if index < 0 or index >= getKeysCount()
 	 */
-	GradientKey getKey(int index);
+	GradientKey getKey(unsigned int index);
 
 	
 	/**
 	 * Sets specific key to specific values.
 	 * If index not exists then do nothing.
 	 */
-	void setKey(int index, float position, sf::Color color);
-	void setKey(int index, float position);
-	void setKey(int index, sf::Color color);
+	void setKey(unsigned int index, float position, sf::Color color);
+	void setKey(unsigned int index, float position);
+	void setKey(unsigned int index, sf::Color color);
 
 	/**
 	 * Copy all keys to array
@@ -68,6 +73,25 @@ public:
 	/**
 	 * Removes key at specific index or do nothing if index is out of bounds
 	 */
-	void removeKey(int index);
+	void removeKey(unsigned int index);
 
+	/**
+	 * @returns index of closest left key to specific position
+	 */
+	unsigned int getLeftKeyIndexFor(float position);
+
+	/**
+	 * Calcs gradient color at specific position
+	 * @param position value to evaluate (for <0 is 0; for >1 is 1 )
+	 * @returns color for passed value
+	 */
+	sf::Color evaluate(float position);
+
+	/**
+	 * Alias for evaluate(float)
+	 * @see #evaluate(float)
+	 */
+	inline sf::Color colorAt(float position){
+		return evaluate(position);
+	}
 };
